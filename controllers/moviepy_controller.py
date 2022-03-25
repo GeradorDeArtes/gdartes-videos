05fe0058da7c9f6833e54d91b78e66b0ac459306
rnsdev <rnsbrum@gmail.com>
2022-03-25 11:07:33 -0300

    fixed resize

--- controllers/moviepy_controller.py
+++ controllers/moviepy_controller.py
@@ -16,7 +16,9 @@ class MoviepyController:
         """
         Needs dynamic size
         """
-        video = VideoFileClip(videoPath, audio=True)
+        video = VideoFileClip(videoPath, audio=False)
+        #audio = video.audio
+
         logo = (ImageClip(imagePath)
                 .set_duration(video.duration)
                 .resize(height=200) # if you need to resize...
@@ -31,35 +33,44 @@ class MoviepyController:
         #return send_from_directory(publicdir, path=file_name, as_attachment=True)
 
     def positionVideoInsideImage(self, videoPath, imagePath, positionX, positionY, componentHeight, componentWidth):
-        """
-            FUCK OFF
-        """
-        video = (VideoFileClip(videoPath, audio=False,)
-                    .set_position((positionX, positionY)))
+        video = (VideoFileClip(videoPath).set_position((positionX, positionY)))
+        audio = video.audio
+        audio = CompositeAudioClip([audio])
 
         #width = crop(originalClip, width=h, height=h, x_center=w/2, y_center=h/2) - WIDTH = 
         #set position can always CENTER a video
         (w, h) = video.size
-        print("component size: ", componentHeight, componentWidth)
-        print("original video size: ", video.w, video.h)
-        video = video.resize(width=componentWidth, height= componentHeight)
-        print("resized video size: ", video.w, video.h)
+        
+        print("component size W-H:", componentWidth, componentHeight)
+        print("original video size W-H:", video.w, video.h)
+
+        videoSizeSum = float(video.w) + float(video.h)
+        componentSizeSum = float(componentWidth) + float(componentHeight)
+
+        print("ComponentSize sum: " +  str(componentSizeSum))
+        print("VideoSize sum: " + str(videoSizeSum))
+
+        video = video.resize(newsize=(componentWidth, componentHeight))
+        print("resized video size W-H: ", video.w, video.h)
 
         if(video.h > componentHeight and video.w > componentWidth ):
             video = crop(video, height=video.h, width=video.w, x_center=w/2, y_center=h/2)
             print("cropped video size: ", video.w, video.h)
+
         image = ImageClip(imagePath, transparent=True).set_duration(video.duration)
-        print("image size: ", image.w, image.h)
+        print("image size W-H: ", image.w, image.h)
         size = (image.w, image.h)
+        
         #create video with image
         #position actual video inside image_video
         final = CompositeVideoClip([ video, image ], size=size)
+        final.audio = audio
+
         file_name =  datetime.now().strftime("%H:%M:%S") + ".mp4"
         final_path = publicdir + file_name
-        final.write_videofile(final_path, fps=24) 
+        final.write_videofile(final_path, audio=False) 
 
         return file_name	
-
 
     def test():
         return 1
